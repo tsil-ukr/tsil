b24c0cc5c7c918628d47180ed52dfc07a0a5e10b
Davyd Kohut <davyd.kohut@gmail.com>
2025-11-01 23:59:15 +0200

    🧬

--- external/tsil.cpp
+++ external/tsil.cpp
@@ -9,8 +9,6 @@ extern "C" {
 
 extern "C" void* д_константа_Т8_розмір = nullptr;
 extern "C" void* д_константа_Т8_дані = nullptr;
-extern "C" void* д_константа_Т8_т8 = nullptr;
-extern "C" void* д_константа_Т8_ю8 = nullptr;
 extern "C" void* д_константа_Т8_ніщо = nullptr;
 extern "C" void* д_константа_Т8_адреса = nullptr;
 extern "C" void* д_константа_Т8_невідома_адреса = nullptr;
